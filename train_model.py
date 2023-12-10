75a59c67c743b27b437738a7453cd358c60f4fde
Ouazraouimeryem <119755467+Ouazraouimeryem@users.noreply.github.com>
2023-12-10 14:19:25 +0100

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,77 +1,130 @@
-import pickle
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from tensorflow.keras.optimizers import Adam
-
-# Charger les données prétraitées
-words = pickle.load(open('words.pkl', 'rb'))
-classes = pickle.load(open('classes.pkl', 'rb'))
 import nltk
+nltk.download('punkt')#Sentence tokenizer
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-from nltk.stem import WordNetLemmatizer
+import warnings
+warnings.filterwarnings('ignore')
 
-nltk.download('punkt')
-nltk.download('wordnet')
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+import random
 
-# Charger les données
-data_file = open('/app/intents.json').read()
-intents = json.loads(data_file)
+"""# **Preprocessing**"""
 
-# Initialiser les variables
-words = []
+words=[]
 classes = []
 documents = []
 ignore_words = ['?', '!']
-lemmatizer = WordNetLemmatizer()
+data_file = open('/app/intents.json').read() # read json file
+intents = json.loads(data_file) # load json file
 
-# Prétraitement des données
+"""When working with text data, we need to perform various preprocessing on the data before we make a machine learning or a deep learning model. Based on the requirements we need to apply various operations to preprocess the
+data.
+- Tokenizing is the most basic and first thing you can do on text data.
+- Tokenizing is the process of breaking the whole text into small parts like words.
+- Here we iterate through the patterns and tokenize the sentence using nltk.word_tokenize() function and append each word in the words list. We also create a list of classes for our tags.
+"""
+
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         #tokenize each word
         w = nltk.word_tokenize(pattern)
-        words.extend(w)  # add each element into list
+        words.extend(w)# add each elements into list
         #combination between patterns and intents
-        documents.append((w, intent['tag']))  # add single element to end of list
+        documents.append((w, intent['tag']))#add single element into end of list
         # add to tag in our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# Créer les données d'entraînement
+nltk.download('wordnet') #lexical database for the English language
+
+nltk.download('omw-1.4')
+
+"""Now we will lemmatize each word and remove duplicate words from the list.
+- Lemmatizing is the process of converting a word into its lemma form and then creating a pickle file to store the Python objects which we will use while predicting.
+"""
+
+# lemmatize, lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+# sort classes
+classes = sorted(list(set(classes)))
+# documents = combination between patterns and intents
+print (len(documents), "documents\n", documents, "\n")
+# classes = intents[tag]
+print (len(classes), "classes\n", classes, "\n")
+# words = all words, vocabulary
+print (len(words), "unique lemmatized words\n", words, "\n")
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+"""# **Training Model**
+
+Now, we will create the training data in which we will provide the input and the output.
+- Our input will be the pattern and output will be the class our input pattern belongs to. But the computer doesn’t understand text so we will convert text into numbers
+"""
+
+# create our training data
 training = []
+# create an empty array for our output
 output_empty = [0] * len(classes)
-
+# training set, bag of words for each sentence
 for doc in documents:
+    # initialize our bag of words
     bag = []
+    # list of tokenized words
     pattern_words = doc[0]
+    # convert pattern_words in lower case
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-
+    # create bag of words array,if word match found in current pattern then put 1 otherwise 0.[row * colm(263)]
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
+    # in output array 0 value for each tag ang 1 value for matched tag.[row * colm(8)]
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
 
     training.append([bag, output_row])
-
-# Mélanger l'entraînement et le transformer en np.array
-np.random.shuffle(training)
+# shuffle training and turn into np.array
+random.shuffle(training)
 training = np.array(training)
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+# create train and test. X - patterns(words), Y - intents(tags)
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data created")
 
-# Créer le modèle
+from tensorflow.python.framework import ops
+ops.reset_default_graph()
+
+"""# **Build the model**
+
+We have our training data ready, now we will build a deep neural network that has 3 layers. We use the Keras sequential API for this. After training the model for 200 epochs, we achieved 100% accuracy on our model. Let us save the model as ‘chatbot_model.h5'.
+"""
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
+print("First layer:",model.layers[0].get_weights()[0])
 
-# Compiler et entraîner le modèle
-model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Sauvegarder le modèle
+#fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.h5')
+
+print("model created")
+
