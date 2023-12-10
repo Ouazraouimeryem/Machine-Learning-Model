385d95f2cb18b4294ff6005e2488dfcf77407678
Ouazraouimeryem <119755467+Ouazraouimeryem@users.noreply.github.com>
2023-12-10 14:08:32 +0100

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -7,6 +7,36 @@ from tensorflow.keras.optimizers import Adam
 # Charger les données prétraitées
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
+import nltk
+import json
+import pickle
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+# Charger les données
+data_file = open('/app/intents.json').read()
+intents = json.loads(data_file)
+
+# Initialiser les variables
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!']
+lemmatizer = WordNetLemmatizer()
+
+# Prétraitement des données
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        #tokenize each word
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)  # add each element into list
+        #combination between patterns and intents
+        documents.append((w, intent['tag']))  # add single element to end of list
+        # add to tag in our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
 
 # Créer les données d'entraînement
 training = []
